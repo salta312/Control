78434bad67fdb44bcdae9dd3b41de7e07cbe6f90
salta312 <salta312@gmail.com>
2016-10-17 21:39:31 +0600

    Initial Commit

---
+++ SelfControl2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfControl2
+//
+//  Created by Saltanat Aimakhanova on 10/17/16.
+//  Copyright © 2016 saltaim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
